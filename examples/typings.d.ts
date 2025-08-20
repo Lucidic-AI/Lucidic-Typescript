a245daef587ee77f3c585e4734a0b975c4094b73
AnvitSinha <anvitsinha@gmail.com>
2025-08-19 19:45:02 -0700

    remove modules not used

--- examples/typings.d.ts
+++ examples/typings.d.ts
@@ -5,6 +5,6 @@ declare module '@ai-sdk/openai';
 declare module '@traceloop/instrumentation-google-generativeai';
 declare module '@traceloop/instrumentation-bedrock';
 declare module '@traceloop/instrumentation-cohere';
-declare module '@traceloop/instrumentation-mistral';
-declare module '@traceloop/instrumentation-groq';
+declare module '@traceloop/instrumentation-vertexai';
+
 
